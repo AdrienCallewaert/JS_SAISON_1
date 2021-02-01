1f2d31a3ce9b96ab99e6aa076577f8dfa378a041
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-01 15:55:50 +0100

    exo 2;3

--- 02-maths/03-even-square/script.js
+++ 02-maths/03-even-square/script.js
@@ -1,3 +1,4 @@
+// it's ok, captain!
 
 // 02-maths/03-even-square/script.js - 2.3: squares and pairs
 
@@ -7,7 +8,12 @@
     document.getElementById("run").addEventListener("click", () => {
 
         // your code here
+        let a = 0;
+        for (let i = 0; i < 21; i++) {
+            alert(Math.pow(i + 1, 2));
 
+        }
     });
 
 })();
+
