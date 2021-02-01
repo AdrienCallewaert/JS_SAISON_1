579a3486244dedfd98263198a8c0981309f0ec0b
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-01 15:18:23 +0100

    suite exo 21 ET 2.2

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -1,4 +1,6 @@
 
+// it's ok, captain!
+
 // 02-maths/01-calculator-one/script.js - 2.1: calculator1
 
 (() => {
@@ -6,17 +8,33 @@
 
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let operandOne = new Number(document.getElementById("op-one").value)
+        let operandTwo = new Number(document.getElementById("op-two").value)
+        let resultat = operandOne + operandTwo
+        return alert(resultat)
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let operandOne = new Number(document.getElementById("op-one").value)
+        let operandTwo = new Number(document.getElementById("op-two").value)
+        let resultat = operandOne - operandTwo
+        return alert(resultat)
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let operandOne = new Number(document.getElementById("op-one").value)
+        let operandTwo = new Number(document.getElementById("op-two").value)
+        let resultat = operandOne * operandTwo
+        return alert(resultat)
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let operandOne = new Number(document.getElementById("op-one").value)
+        let operandTwo = new Number(document.getElementById("op-two").value)
+        let resultat = operandOne / operandTwo
+        return alert(resultat)
     });
 })();
--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -1,17 +1,34 @@
 
+// it's ok, captain!
+
 // 02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
 
 (() => {
-    // to get the value of an input: document.getElementById("element-id").value
+  // to get the value of an input: document.getElementById("element-id").value
 
-    const performOperation = operation => {
-        // perform the operation
-    };
+  const performOperation = operation => {
+      // perform the operation
+      switch (operation) {
+          case 'addition':
+              window.alert((new Number(document.getElementById("op-one").value)) + (new Number(document.getElementById("op-two").value)));
+              break;
+          case 'substraction':
+              window.alert((new Number(document.getElementById("op-one").value)) - (new Number(document.getElementById("op-two").value)));
+              break;
+          case 'multiplication':
+              window.alert((new Number(document.getElementById("op-one").value)) * (new Number(document.getElementById("op-two").value)));
+              break;
+          case 'division':
+              window.alert((new Number(document.getElementById("op-one").value)) / (new Number(document.getElementById("op-two").value)));
+              break;
+      }
+  };
 
-    Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
-        $btn.addEventListener(
-            "click",
-            () => (performOperation($btn.id), false),
-        ),
-    );
+  Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
+      $btn.addEventListener(
+          "click",
+          () => (performOperation($btn.id), false),
+      ),
+  );
 })();
+
