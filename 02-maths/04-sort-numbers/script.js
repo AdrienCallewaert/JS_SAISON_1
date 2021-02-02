3f8972578c5c9e4d8876ddce8516c58db1610cc6
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-02 08:47:49 +0100

    up 2.4

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -1,13 +1,23 @@
+// it's ok, captain!
+
 // 02-maths/04-sort-numbers/script.js - 2.4: Sorting the numbers
 
 (() => {
 
     // to get the value of an input: document.getElementById("element-id").value
+    
+    let array = document.getElementById("numbers").value.split(",");
 
     document.getElementById("run").addEventListener("click", () => {
+    
+    alert(array.sort((function(a, b){return a-b})));
+
 
         // your code here
 
     });
 
 })();
+
+
+
