f8d76d03b804d8ec3507229da443ec28cf29b794
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-02 09:21:57 +0100

    2.5

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -1,3 +1,4 @@
+// it's ok, captain!
 
 // 02-maths/05-factorial/script.js - 2.5: Factorial
 
@@ -6,6 +7,18 @@
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", () => {
+        let n = new Number (document.getElementById("number").value)
+    
+        function factorial(n){
+            if(n == 0 || n == 1){
+                return 1;
+            }
+            else{
+                return n * factorial(n-1);
+            }
+        }
+        alert (factorial(n))
+
 
         // your code here
 
