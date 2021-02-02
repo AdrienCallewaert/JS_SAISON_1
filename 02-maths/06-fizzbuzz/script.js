50845e7ac4d11d1db567c27c24953a5d3171fdbf
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-02 10:16:24 +0100

    2.6

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -5,4 +5,20 @@
 
     // your code here
 
+    let i = 1;
+
+    while (i <= 100) {
+        if (i % 3 === 0) {
+            console.log("fizz");
+        }
+        if ( i % 5 === 0) {
+            console.log("buzz");
+        }
+        if (i % 3 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        }
+        console.log(i);
+        i += 1;
+    }
+
 })();
