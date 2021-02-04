ace1d5917df5a7a5d19823d6060bebb8334f0db2
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-04 16:50:22 +0100

    5;4

--- 05-arrays/14-bird-names-generator/script.js
+++ 05-arrays/14-bird-names-generator/script.js
@@ -1,3 +1,5 @@
+// it's ok, captain!
+
 // 05-arrays/14-bird-names-generator/script.js - 5.14: Bird names generator in French
 
 
@@ -31,4 +33,26 @@
     ]);
 
     // your code here
+
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        let bird = birds[Math.floor(Math.random()*12)-1];
+        let adj = adjectives.values();
+        let adjectif;
+
+        for (let index = 0; index < Math.floor(Math.random() * (adjectives.size)); index++) {
+            adjectif = adj.next().value;
+        }
+
+            if(bird.fem) {
+                document.getElementById("target").innerHTML = "La " + bird.name + " " + adjectif + "e."
+            }
+
+            else{
+                document.getElementById("target").innerHTML = "Le " + bird.name + " " + adjectif + "."
+            }
+
+    }) 
+
+
 })();
