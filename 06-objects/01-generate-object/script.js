92fd2b11989494c2f1f09bbe1ef8aec0945a8b59
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-04 16:50:36 +0100

    6.1

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -1,6 +1,23 @@
+// it's ok, captain!
+
 // 06-objects/01-generate-object/script.js - 6.1: genererate an object
 
 
 (() => {
     // your code here
+
+
+    document.getElementById("run").addEventListener("click", ()=>{
+
+        let person = {
+            firstName : "Adrien",
+            lastName : "Callewaert",
+            age : 31,
+            city : "Namur",
+            country : "Belgique"
+          };
+
+        alert("Hello, je suis " + person.firstName + " " + person.lastName + ". J'ai " + person.age + " ans, j'habite " + person.city + " en " + person.country + ".");
+
+    }) 
 })();
