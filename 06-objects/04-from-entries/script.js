95741b1915eb4e62ee910d3e68040c241bc41c77
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-05 10:36:49 +0100

    6;4

--- 06-objects/04-from-entries/script.js
+++ 06-objects/04-from-entries/script.js
@@ -1,8 +1,24 @@
+// it's ok, captain!
+
 // 06-objects/04-from-entries/script.js - 6.4: fromEntries
 
 
 (() => {
     const keys = ["name", "species", "age", "gender", "color"];
-    const values = ["Skitty", "cat", 9, "female", "tabby"];
+    const values = ["Happy", "cat", 9, "female", "garbage"];
     // your code here
+
+    document.getElementById("run").addEventListener("click", () => {
+        
+        let Arr = [];
+
+        keys.forEach((newKeys, newValues) => {
+            Arr.push([newKeys, values[newValues]]);
+        });
+
+        let nObject = Object.fromEntries(Arr);
+
+        console.log(nObject);
+    });
+
 })();
