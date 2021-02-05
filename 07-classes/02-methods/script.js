f9e076d7495a1ecdf62dd69a0e2620ccb788cc7d
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-05 12:31:20 +0100

    7.2

--- 07-classes/02-methods/script.js
+++ 07-classes/02-methods/script.js
@@ -1,6 +1,21 @@
+// it's ok, captain!
+
 //07-classes/02-methods/script.js - 7.2: methods
 
 
 (() => {
     // your code here
+    class Person {
+        constructor(firstname,lastname){
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+        sayHello(){
+            return "Hello, " + this.firstname + " " + this.lastname + "!";
+        }
+    }
+    document.getElementById("run").addEventListener("click",()=>{
+        let onePerson = new Person("Adrien","Callewaert");
+        console.log(onePerson.sayHello());
+    })
 })();
