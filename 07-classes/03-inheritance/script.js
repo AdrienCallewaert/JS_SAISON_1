3c54e0f3dde9592a93758c9f2657b0c9b90874c0
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-05 12:32:53 +0100

    7.3

--- 07-classes/03-inheritance/script.js
+++ 07-classes/03-inheritance/script.js
@@ -1,3 +1,5 @@
+// it's ok, captain!
+
 // 07-classes/03-inheritance/script.js - 7.3: inheritance
 
 
@@ -8,4 +10,31 @@
         }
     }
     // your code here
+
+    class Cat extends Animal {
+        constructor(name) {
+            super();this.name = name;
+        }
+        static greeting = "get out human ";
+
+    }
+
+    class Dog extends Animal {
+        constructor(name) {
+            super();this.name = name;
+        }
+        static greeting = "welkome human ";
+
+    }
+    
+    document.getElementById("run").addEventListener("click", ()=>{
+        let firstcat = new Cat("Happy");
+        let firstdog = new Dog("Quaonne");
+
+
+
+        console.log(firstcat.sayHello());
+        console.log(firstdog.sayHello());
+
+    }) 
 })();
