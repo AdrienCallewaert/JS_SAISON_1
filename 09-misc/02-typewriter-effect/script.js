b008a04e70700d2f3843ff4bfd238f649d979f42
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-12 16:57:14 +0100

    Update script.js

--- 09-misc/02-typewriter-effect/script.js
+++ 09-misc/02-typewriter-effect/script.js
@@ -1,3 +1,5 @@
+// it's ok, captain! ou pas hein
+
 // 09-misc/02-typewriter-effect/script.js - 9.2: typewriter effect
 
 
@@ -5,4 +7,23 @@
 
     // your code here
 
+
+    let i = 0;
+    let txt = document.getElementById('target').textContent;
+    let speed = Math.floor(Math.random()*txt.length); 
+    
+    function Supp(){
+         document.getElementById('target').innerHTML = ''
+    }
+    
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("target").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    Supp()
+    typeWriter()
 })();
+
