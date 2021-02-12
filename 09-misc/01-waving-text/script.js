7687c9ae0465815be7c0c89dbeea53396069f685
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-12 16:57:02 +0100

    Update script.js

--- 09-misc/01-waving-text/script.js
+++ 09-misc/01-waving-text/script.js
@@ -1,8 +1,21 @@
+// it's ok, captain! ou pas hein
+
 // 09-misc/01-waving-text/script.js - 9.1: waving text
 
 
 (() => {
 
     // your code here
+ 
 
+    function size(){
+    let size = ["x-small","small","medium","large","x-large"];
+    let fontsize = Math.floor(Math.random() * size.length);
+    return size[fontsize]
+    }
+    
+    let span = document.getElementById('target').textContent.split('').map( lettre => `<span class ='`+ lettre +`' style ='font-size:` +size()+ `' >`+ lettre +'</span>').join('');
+    
+    return document.getElementById('target').innerHTML = span
+        
 })();
