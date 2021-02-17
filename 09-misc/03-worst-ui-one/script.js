bd0f2cbb67ca8228d0c7f2cda45e1b06afa7fd49
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-02-17 09:16:36 +0100

    Update script.js

--- 09-misc/03-worst-ui-one/script.js
+++ 09-misc/03-worst-ui-one/script.js
@@ -1,8 +1,16 @@
+// it's ok, captain! ou pas hein
+
 // 09-misc/03-worst-ui-one/script.js - 9.3: worst user interface (1) : phone slider
 
 
 (() => {
 
     // your code here
+
+    let slider = document.getElementById("slider");
+
+    slider.addEventListener("mouseup", () => {
+        document.getElementById("target").innerHTML = `0${slider.value}`;
+    });
 
 })();
